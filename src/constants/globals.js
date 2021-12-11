b03ea67b7cfab7caf058e1befa80a671db739c1c
GallegosEverson <gallegoseverson@gmail.com>
2021-12-11 15:21:38 -0300

    url config

--- src/constants/globals.js
+++ src/constants/globals.js
@@ -1,4 +1,4 @@
-export const URL = 'http://127.0.0.1:8000/'
+export const URL = 'https://betshare-app.herokuapp.com/'
 export const endpoint_games = 'api/games/'
 export const endpoint_send_request = 'api/profile/request/'
 export const endpoint_cart = 'api/profile/cart/'
